ce7826b70f042326e1de876bb4e9ca0d058bc5d0
Peter deHaan <peter@deseloper.com>
2023-10-01 14:48:47 -0700

    Add debug module

--- lib.js
+++ lib.js
@@ -1,7 +1,10 @@
 import fs from "node:fs";
 import path from "node:path";
+import d from "debug";
 import fg from "fast-glob";
 import inclusive from "@11ty/eleventy-plugin-inclusive-language/inclusive-language.js";
+
+const debug = d("inclusive");
 
 export let WORDS =
   "simply,obviously,basically,of course,clearly,just,everyone knows,however,easy".split(
@@ -10,15 +13,19 @@ export let WORDS =
 
 export function setWords(words = []) {
   WORDS = normalizeList(words);
+  debug("setWords = %o", WORDS);
 }
 
 export function appendWords(words = []) {
   WORDS = WORDS.concat(normalizeList(words));
+  debug("appendWords = %o", WORDS);
 }
 
 export function lint(...globs) {
+  debug("globs = %o", globs);
   return globs.reduce((acc, g) => {
     const files = fg.globSync(path.join(process.cwd(), g));
+    debug("files = %o", files);
     for (const f of files) {
       const contents = fs.readFileSync(f, "utf-8");
       const file = path.relative(process.cwd(), f);
